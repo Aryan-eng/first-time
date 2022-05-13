7d2f14bdd865785c576dfce125e13545378151d1
“Aryan <72766369+Aryan-eng@users.noreply.github.com>
2022-05-12 21:14:10 -0400

    init

---
+++ test.py
@@ -0,0 +1,9 @@
+def prime(n):
+  for i in range(2,n):
+    if n%i == 0:
+      print("This number is not prime")
+      break
+    else:
+      print("This number is prime")
+      break
+prime(157)
